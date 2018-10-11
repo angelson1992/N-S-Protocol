69a1ff809a918ee67869838626ae027bd65e46c0
John <Angelson1992@gmail.com>
2018-10-11 05:22:03 -0400

    Fully working HW assignment with clutter taken out

--- src/Main.java
+++ src/Main.java
@@ -8,21 +8,6 @@ public class Main {
 
   public static void main(String[] args) throws IOException, InterruptedException {
 
-    DiffieHellman_Key_Exchange KeAlice = new DiffieHellman_Key_Exchange(353, 3);
-
-    DiffieHellman_Key_Exchange KeBob = new DiffieHellman_Key_Exchange(353, 3);
-
-    int alicePublicKey = KeAlice.getPublicKey();
-    int bobPublicKey = KeBob.getPublicKey();
-
-    System.out.println("Alice says that session key is " + KeAlice.computeSessionKey(bobPublicKey) + " and Bob says the session key is " + KeBob.computeSessionKey(alicePublicKey));
-
-    Toy_DES_CounterVersion counterDES = new Toy_DES_CounterVersion(Integer.toString((int)System.currentTimeMillis()%255, 2));
-    int[] test = {1, 2, 231};
-    int[] cypher = counterDES.encryptAndDecrypt(test, true);
-    System.out.println(Arrays.toString(cypher));
-    System.out.println(Arrays.toString(counterDES.encryptAndDecrypt(cypher, false)));
-
     Server keyDistCntr = new Server(353, 3, 2018);
     Client alice = new Client(353, 3, 2018, 18881, "Alice", true);
     sleep(500);
